853231ae4d3feaa630951cbad600c291285a9855
manduMY <poi31320@naver.com>
2020-05-13 13:17:55 +0900

    SQLite 사용을 위한 Room 만들기
     SQLite(DataBase)를 사용하기 위해 Room(Note.class)을 만들고 기본키
    설정.
    
    테이블의 컬럼 또는 필드를 위한 인스턴스 변수(id, title, description,
    priority) 선언.

--- app/src/main/java/com/example/mvvmarchitecture/MainActivity.java
+++ app/src/main/java/com/example/mvvmarchitecture/MainActivity.java
@@ -1,34 +1,14 @@
 package com.example.mvvmarchitecture;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModel;
-import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
-import android.widget.Toast;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private ViewModelProvider.AndroidViewModelFactory viewModelFactory;
-    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if(viewModelFactory == null) {
-            viewModelFactory = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication());
-        }
-        noteViewModel = new ViewModelProvider(this, viewModelFactory).get(NoteViewModel.class);
-        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
-            @Override
-            public void onChanged(List<Note> notes) {
-                // update RecyclerView
-                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
--- app/src/main/java/com/example/mvvmarchitecture/Note.java
+++ app/src/main/java/com/example/mvvmarchitecture/Note.java
@@ -4,17 +4,18 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+// Room(Note.class)
+// SQLite(데이터 베이스)를 사용하기 위해 Room을 추가 했습니다.
 @Entity(tableName = "note_table")
 public class Note {
-
+    // Room에서 id에 자동 ID를 할당하기 위해 autoGenerate를 true로 설정.
     @PrimaryKey(autoGenerate = true)
     private int id;
 
-    private  String title;
+    private String title;
 
     private String description;
 
-    @ColumnInfo(name = "priority")
     private int priority;
 
     public Note(String title, String description, int priority) {
