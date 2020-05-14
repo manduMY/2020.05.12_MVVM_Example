da8b47e4d973d30aa3da3e0a037373b1fd4f6dca
manduMY <poi31320@naver.com>
2020-05-15 05:05:17 +0900

    RecyclerView + ADAPTER
     Activity에 데이터베이스 테이블의 항목을 표시하기 위해 RecyclerView를
    설정할 것입니다.
    이를 위해 먼저 단일 CardView 행에 대한 XML 레이아웃을 준비(note_item.xml)한 다음RecyclerView.Adapter를 생성하여 ViewHolder를 추가합니다. 내부 클래스로 Holder 및onCreateViewHolder 구현, onBindViewHolder 구현과 getItemCount 메서드로 데이터가 추가되는지 확인 하십시요.
    LiveData의 OnChanged 콜백에서 ArrayList를 업데이트하려면 새 List를 통과한 어댑터에 다른 공개 방법을 추가하고 DataSetChanged를 호출하십시오.

--- app/src/main/java/com/example/mvvmarchitecture/MainActivity.java
+++ app/src/main/java/com/example/mvvmarchitecture/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.mvvmarchitecture;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -16,6 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // *Recycler View*
+        // 1. RecyclerView는 "많은 수의 데이터 집합을, 제한된 영역 내에서 유연하게(flexible) 표시할 수 있도록 만들어주는 위젯"입니다.
+        // 2. Detail하게 말하자면 RecyclerView는 "사용자가 관리하는 많은 수의 데이터 집합(Data Set)을 개별 아이템 단위로 구성하여 화면에
+        //    출력하는 뷰그룹(ViewGroup)이며, 한 화면에 표시되기 힘든 많은 수의 데이터를 스크롤 가능한 리스트로 표시해주는 위젯"입니다.
+        // 3. 리스트 뷰의 경우 리스트 항목이 갱신될 때마다 매번 item view를 새로 구성해야 하는데 "많은 수의 데이터 집합을 표시"하는데 있어서
+        //    성능 저하가 야기될 수 있습니다. 이러한 리스트뷰의 단점을 보완하기 위해 Recycler View는 아이템을 재활용 합니다.
+        //    그리고 이를 위해 기본적으로 ViewHolder 패턴을 사용하도록 만들어 놓아야 합니다. ViewHolder는 필수 구현 사항
+        // 4. ViewHolder를 통해 RecyclerView가 유연해집니다.
+        // 5. DataList(ex. item1,item2,item3 ...) -> Adapter -> Recycler View(뿌려줌)
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        // LayoutManager를 통해 DataList(item1, item2, item3 ...)를 수직으로 Layout 배치를 해줍니다.
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        // ViewHolder를 위해 adapter를 만듬.
+        final NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
 
         // Activity에서는 시스템이 ViewModelProviders를 호출하여 올바른 ViewModel 인스턴스를 제공하도록 하고, 여기서 우리는 이 ViewModel의 라이프사이클의 범위를 지정해야 하는 Activity 또는 Fragment를 통과시킨다.
         // Activity/Fragment가 파괴되면, ViewModel은 OnCleared 방법을 통해 메모리에서 제거될 것이다.
@@ -31,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
                 // onChange 콜백에서는 해당 데이터베이스 테이블의 무언가가 변경될 때마다 데이터에 대한 업데이트를 받는다.
                 // LiveData는 라이프사이클에서 적절한 시점에 자동으로 업데이트 전송을 시작 및 중지하고 사용되지 않는 참조를 정리한다.
                 // update RecyclerView
-                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+                adapter.setNotes(notes);
             }
         });
     }
---
+++ app/src/main/java/com/example/mvvmarchitecture/NoteAdapter.java
@@ -0,0 +1,64 @@
+package com.example.mvvmarchitecture;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 1. Recycler View 아이템 추가, 수정, 삭제하기 위해 Adapter를 만들었습니다.
+// 2. 사용자 데이터 리스트로부터 아이템 뷰를 만드는 것이 어댑터가 하는 역할입니다.
+// 3. 어댑터를 통해 만들어진 각 아이템 뷰는 "뷰홀더(ViewHolder)"객체에 저장되어 화면에 표시되고, 필요에 따라 생성 또는 재활용(Recycle)됩니다.
+// 4. ViewHolder란 화면에 표시될 아이템 뷰를 저장하는 객체입니다.
+// 5. 어댑터에 의해 관리되며 필요에 따라 어댑터에서 생성.
+//    물론, 미리 생성된 뷰홀더 객체가 있는 경우에는 새로 생성하지 않고 이미 만들어져 있는 뷰홀더를 재활용하는데,
+//    이 때는 단순히 데이터가 뷰홀더의 아이템 뷰에 바인딩(Binding) 됩니다.
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+    private List<Note> notes = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.note_item, parent, false);
+        return new NoteHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
+        // 생성된 뷰홀더에 데이터를 바인딩 해주는 함수이다.
+        Note currentNote = notes.get(position);
+        holder.textviewTitle.setText(currentNote.getTitle());
+        holder.textViewDescription.setText(currentNote.getDescription());
+        holder.textViewPriority.setText(String.valueOf(currentNote.getPriority()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+        // Recycler View 갱신
+        notifyDataSetChanged();
+    }
+    class NoteHolder extends RecyclerView.ViewHolder {
+        private TextView textviewTitle;
+        private TextView textViewDescription;
+        private TextView textViewPriority;
+
+        public NoteHolder(View itemView) {
+            super(itemView);
+            textviewTitle = itemView.findViewById(R.id.text_view_title);
+            textViewDescription = itemView.findViewById(R.id.text_view_description);
+            textViewPriority = itemView.findViewById(R.id.text_view_priority);
+        }
+    }
+}
